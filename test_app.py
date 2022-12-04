a95f366edddc1b048cb9738f5693ddc592d3e314
Milo van Emaus <38001005+milodepilo@users.noreply.github.com>
2022-12-04 17:53:18 +0100

    fixed typo

--- test_app.py
+++ test_app.py
@@ -6,7 +6,7 @@ def client():
     client = app.test_client()
     return client
 
-def test_index(clieny):
+def test_index(client):
     response = client.get("/")
     assert response.status_code == 200
     assert b"Hello" in response.data
