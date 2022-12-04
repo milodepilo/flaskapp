d205859ef0289e0706434c70d42cbce7aeedf799
Milo van Emaus <38001005+milodepilo@users.noreply.github.com>
2022-12-04 16:39:24 +0100

    added first files

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 # Import what we need from flask
-from flask import Flask
+from flask import Flask, render_template, redirect
 
 # Create a Flask app inside `app`
 app = Flask(__name__)
 
 # Assign a function to be called when the path `/` is requested
-@app.route('/')
+@app.route('/', methods=["GET"])
 def index():
     return 'Hello, world!'
 
 @app.route('/cow')
 def cow():
     return 'MOoooOo!'
+
+
---
+++ test_app.py
@@ -0,0 +1,15 @@
+from main import app
+import pytest
+
+@pytest.fixture
+def client():
+    client = app.test_client()
+    return client
+
+def test_index(clieny):
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b"Hello" in response.data
+
+
+
