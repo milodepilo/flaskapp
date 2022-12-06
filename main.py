9f33bdb29dcca526bb3192e314dec42f6bfbf74e
Milo van Emaus <38001005+milodepilo@users.noreply.github.com>
2022-12-06 09:13:18 +0100

    added redirect from home

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 # Import what we need from flask
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, url_for
 
 # Create a Flask app inside `app`
 app = Flask(__name__)
 
 # Assign a function to be called when the path `/` is requested
+
+
+@app.route("/home", methods=["GET"])
+def home():
+    return redirect(url_for("index"), code=302)
+
+
 @app.route('/', methods=["GET"])
 def index():
     return render_template("index.html", title="Index")
--- test_app.py
+++ test_app.py
@@ -1,18 +1,26 @@
 from main import app
 import pytest
 
+
 @pytest.fixture
 def client():
     client = app.test_client()
     return client
+
+
+def test_redirect(client):
+    response = client.get("/home")
+    assert response.status_code == 302
+    assert response.location == "/"
+
 
 def test_index(client):
     response = client.get("/")
     assert response.status_code == 200
     assert b"<title>Index</title>" in response.data
 
+
 def test_about(client):
     response = client.get("/about")
     assert response.status_code == 200
     assert b"<title>about</title>" in response.data
-
