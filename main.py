f16e8c9bf6bd06087005b42558a0498c7bbcaf50
Milo van Emaus <38001005+milodepilo@users.noreply.github.com>
2022-12-06 10:25:37 +0100

    changed link to img

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 from flask import Flask, render_template, redirect, url_for
 
 # Create a Flask app inside `app`
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 
 @app.route("/home", methods=["GET"])
